232d804796f03e8d1082e4549f38f14ce8de4a33
Horshenia <leib-horshenia@ukr.net>
2022-09-18 15:27:10 +0300

    Додання дати

--- LR2/src/ua/Paton/Main.java
+++ LR2/src/ua/Paton/Main.java
@@ -2,6 +2,7 @@ package ua.Paton;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("test");
+		System.out.println("ТПJ_ЛР2");
+		System.out.println("18 вересня 2022");
 	}
 }
